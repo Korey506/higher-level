fb5e4d747369778f68127c679d90cd655bdd91c5
korede thompson <koredeodeyemi456@gmail.com>
2024-03-06 15:32:38 +0000

    Added some comments to the Base_model

--- models/base_model.py
+++ models/base_model.py
@@ -17,8 +17,12 @@ class BaseModel():
 
         if kwargs:
             for key, value in kwargs.items():
+
+                """ check if the type(key) is a __class__, skip if yes """
                 if key == "__class__":
                     continue
+
+                """ convert the created_at and updated_at to objects """
                 if key in ("created_at", "updated_at"):
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 setattr(self, key, value)
