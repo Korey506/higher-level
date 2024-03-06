c7b8590235bbcdc282b0c07b2ec70fc8b072f9a2
korede thompson <koredeodeyemi456@gmail.com>
2024-03-06 15:23:20 +0000

    update the base model as required by task 4

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,24 @@ from datetime import datetime
     uuid - for generating universal unique ID
     datetime - to genrate current date and time an instance is created
 """
+
+
 class BaseModel():
     """ Defines all common attributes/methods for othe classes """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ initializes the public instances """
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key in ("created_at", "updated_at"):
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
 
     def __str__(self):
         """ string representation of the BaseModel class """
