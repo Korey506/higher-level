5b3e0d05823b1b1223e01d33b85fa8fd5b99ec23
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-03-12 00:28:50 +0100

    importing

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 """ import json module """
 import json
 from os import path
+from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
