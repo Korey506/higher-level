9c37809c53c696139ab8120745ba5b05e9b8db7f
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-03-11 23:22:51 +0100

    __obj used instead __objects as specific

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,33 +14,33 @@ from models.review import Review
 class FileStorage:
     """ handles the storage of objects into a Json file """
     __file_path = "file.json"
-    __obj = {}
+    __objects = {}
 
     def all(self):
         """ returns a dictionary obj """
-        return self.__obj
+        return self.__objects
 
     def new(self, obj):
         """ sets new object to the dictionary """
         key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__obj[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """ serialize __obj to a JSON file """
         serialized_objects = {}
-        for key, value in self.__obj.items():
+        for key, value in self.__objects.items():
             serialized_objects[key] = value.to_dict()
 
-        with open(self.__file_path, 'w') as file:
-            json.dump(serialized_objects, file)
+        with open(self.__file_path, 'w') as file_:
+            json.dump(serialized_objects, file_)
 
     def reload(self):
         """ Deserialization """
         if path.exists(self.__file_path):
-            with open(self.__file_path, 'r') as file:
-                data = json.load(file)
+            with open(self.__file_path, 'r') as file_:
+                data = json.load(file_)
                 for key, value in data.items():
                     class_name, obj_id = key.split('.')
                     class_type = globals().get(class_name)
                     if class_type:
-                        self.__obj[key] = class_type(**value)
+                        self.__objects[key] = class_type(**value)
