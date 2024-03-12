85959a56d765c02b68c1afe052d9957df80432c9
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-03-12 16:35:41 +0100

    user.email

--- models/user.py
+++ models/user.py
@@ -4,9 +4,8 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    def __init__(self, *args, **kwargs):
-        super().__init__(args, **kwargs)
-        self.email = kwargs.get('email', '')
-        self.password = kwargs.get('password', '')
-        self.first_name = kwargs.get('first_name', '')
-        self.last_name = kwargs.get('last_name', '')
+    """Defines attributes for user creation"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
